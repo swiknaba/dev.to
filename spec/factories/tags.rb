c89b7db8210b4776795016ee0ff6308645d9ecab
mstruve <mollylbs@gmail.com>
2019-12-27 16:44:39 -0600

    by default create tags with valid hex colors

--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
   factory :tag do
     name { generate :name }
     supported { true }
+    text_color_hex { Faker::Color.hex_color }
+    bg_color_hex { Faker::Color.hex_color }
   end
 end
