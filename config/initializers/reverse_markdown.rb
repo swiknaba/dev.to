e3ec9c5fe2824d6db48a587c07bf797f714789be
swiknaba <swiknaba@users.noreply.github.com>
2019-12-31 13:59:21 +0100

    guard clause loading of custom reverse_markdown extensions in dev/test env

--- config/initializers/reverse_markdown.rb
+++ config/initializers/reverse_markdown.rb
@@ -5,6 +5,12 @@
 # applicable in development (and test, when needed)
 
 if Rails.env.development? || Rails.env.test?
+  begin
+    require "reverse_markdown"
+  rescue LoadError
+    return unless defined?(::ReverseMarkdown::Converters::Base)
+  end
+
   Rails.application.config.to_prepare do
     Dir.glob(Rails.root.join("app/lib/reverse_markdown/converters/*.rb")).sort.each do |filename|
       require_dependency filename
